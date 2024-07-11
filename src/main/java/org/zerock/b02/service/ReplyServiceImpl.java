d5de386430937fc16c6d9ad056f744eab9f494ba
0Hoxy <tinyfinger@naver.com>
2024-07-11 11:28:40 +0900

    댓글 조회/수정/삭제/목록

---
+++ src/main/java/org/zerock/b02/service/ReplyServiceImpl.java
@@ -0,0 +1,47 @@
+package org.zerock.b02.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.zerock.b02.domain.Reply;
+import org.zerock.b02.dto.ReplyDTO;
+import org.zerock.b02.repository.ReplyRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class ReplyServiceImpl implements ReplyService {
+  private final ReplyRepository replyRepository;
+  private final ModelMapper modelMapper;
+
+  @Override
+  public Long register(ReplyDTO replyDTO){
+    Reply reply = modelMapper.map(replyDTO, Reply.class);
+    Long rno = replyRepository.save(reply).getRno();
+    return rno;
+  }
+
+  @Override
+  public ReplyDTO read(Long rno){
+    Optional<Reply> replyOptional = replyRepository.findById(rno);
+    Reply reply = replyOptional.orElseThrow();
+    return modelMapper.map(reply, ReplyDTO.class);
+  }
+  
+  @Override
+  public void modify(ReplyDTO replyDTO){
+    Optional<Reply> replyOptional = replyRepository.findById(replyDTO.getRno());
+    Reply reply = replyOptional.orElseThrow();
+    //수정
+    reply.changeText(replyDTO.getReplyText());
+    replyRepository.save(reply);
+  }
+  
+  @Override
+  public void remove(Long rno){
+    Optional<Reply> replyOptional = replyRepository.findById(rno);
+  }
+}
